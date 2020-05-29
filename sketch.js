74ab8bf9366637a05c59551391393b0125e70748
shander3377 <pankaj3377@gmail.com>
2020-05-29 23:24:45 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83)
+  bullet = createSprite(50,200,30,50)
+  bullet.velocityX = speed;
+  wall=createSprite(1500,200,thickness,height/2)
+}
+
+function draw() {
+  background(255,255,255);  
+  if (hasCollided(bullet, wall)) {
+         bullet.velocityX = 0.2;
+         var damage = 0.5 * weight * speed * speed / thickness * thickness * thickness
+         if(damage < 10) {
+           wall.shapeColor = "green";
+         }
+         if(damage > 10) 
+         {
+           bullet.shapeColor = "red"
+         
+         }
+    }
+  drawSprites();
+}
+
+function hasCollided(bullet, wall) {
+  if (wall.x - bullet.x < bullet.width/2 + wall.width/2) {
+    return true 
+  }
+   else {
+      return false
+  }
+}
